ecb52e634d8ea3aa6941c5423d52f27b25585aa1
progmaster001 <104674157+progmaster001@users.noreply.github.com>
2023-10-01 22:43:28 +0500

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,7 @@ class AuthenticatedSessionController extends Controller
         if (Auth::user()->is_admin == '1') {
             return redirect()->route('team.index');
         } else {
-            return redirect('/map-1');
+            return redirect('/dashboard');
         }
     }
 
